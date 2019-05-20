08b812973e308c1a2784a8b04929f01860a0ff53
avrahamMayerfeld <44050391+avrahamMayerfeld@users.noreply.github.com>
2019-05-19 23:08:11 -0400

    fixed indentation

--- Bank3000/src/bank3000pak/Transaction03.java
+++ Bank3000/src/bank3000pak/Transaction03.java
@@ -4,32 +4,31 @@ import javax.swing.JOptionPane;
 
 public class Transaction03 {
 
-private int account;
-private double amount;
-private String date;
+	private int account;
+	private double amount;
+	private String date;
 
 
-public Transaction03( int acct, double amt, String dt)
-{
-account=acct;
-amount=amt;
-date=dt;
-}
+	public Transaction03( int acct, double amt, String dt)
+	{
+		account=acct;
+		amount=amt;
+		date=dt;
+	}
 
-public int getAccount(){
-return account;
-}
-public double getAmount(){
-	return amount;	
-}
-public String getDate(){
-	return date;
-}
+	public int getAccount(){
+		return account;
+	}
+	public double getAmount(){
+		return amount;	
+	}
+	public String getDate(){
+		return date;
+	}
 
-public String printTrans(){
-
-	return ("\n"+String.valueOf(amount)+"\t"+date);
-}
+	public String printTrans(){
+		return ("\n"+String.valueOf(amount)+"\t"+date);
+	}
 }
 
 
