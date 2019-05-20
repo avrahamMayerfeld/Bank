a24fb4c2233330e922631171c7e3198d9cb7ca70
avrahamMayerfeld <44050391+avrahamMayerfeld@users.noreply.github.com>
2019-05-19 22:43:42 -0400

    fixed indentation

--- Bank3000/src/bank3000pak/CDAccount03.java
+++ Bank3000/src/bank3000pak/CDAccount03.java
@@ -1,16 +1,16 @@
 package bank3000pak;
 
 public class CDAccount03 extends Account03 {
-//private String maturityDate;
+	//private String maturityDate;
 
 	public CDAccount03(String n,int aN){
 		super(n, aN);
 	}
 
-/*public void setMaturity(String s){
+	/*public void setMaturity(String s){
 	maturityDate=s;
 
-}
+	}
 	
 	public String getMaturity(){
 	return maturityDate;*/
