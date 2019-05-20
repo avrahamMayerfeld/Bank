bdbb58bcc3ba9aca64ae63126e1a18593b607af1
avrahamMayerfeld <44050391+avrahamMayerfeld@users.noreply.github.com>
2019-05-19 21:53:25 -0400

    fixed github indent corruption

--- Bank3000/src/bank3000pak/Bank03.java
+++ Bank3000/src/bank3000pak/Bank03.java
@@ -10,85 +10,93 @@ public class Bank03 {
 
 	switch(accountType){
 
-	case "Savings":
-	case "savings":{SavingsAccount03 a=new SavingsAccount03(name,number);
-	listOfAccounts.add(a);
-	break;}
-	case "Checking":
-	case "checking":{CheckingAccount03 b=new CheckingAccount03(name,number);
-	listOfAccounts.add(b);
-	break;}
-	case "cd":
-	case "CD":{CDAccount03 c=new CDAccount03(name,number);
-	listOfAccounts.add(c);
-	break;}
-	default:break;
-	}
+		case "Savings":
+		case "savings":{
+			SavingsAccount03 a=new SavingsAccount03(name,number);
+			listOfAccounts.add(a);
+			break;
+		}
+		case "Checking":
+		case "checking":{
+			CheckingAccount03 b=new CheckingAccount03(name,number);
+			listOfAccounts.add(b);
+			break;
+		}
+		case "cd":
+		case "CD":{
+			CDAccount03 c=new CDAccount03(name,number);
+			listOfAccounts.add(c);
+			break;
+		}
+		default:break;
+		}
 
 	}
 	
 	public static void performTransaction(int x,double y, String z)
 	{
-	Transaction03 t=new Transaction03(x,y,z);
+		Transaction03 t=new Transaction03(x,y,z);
 
-	for (int s=0;s<listOfAccounts.size();s++)
-			{
-		if (listOfAccounts.get(s).getAcctNum()==t.getAccount()) 
-	      {
-	   Account03   w= listOfAccounts.get(s);
-	         w.setBalance(t.getAmount());
-		 w.addTransaction(t);
-                    break;
-	      }}
+		for (int s=0;s<listOfAccounts.size();s++)
+		{
+			if (listOfAccounts.get(s).getAcctNum()==t.getAccount()) 
+		      	{
+		   		Account03   w= listOfAccounts.get(s);
+			 	w.setBalance(t.getAmount());
+			 	w.addTransaction(t);
+			    	break;
+		      	}
+		}
 	}
+	
 	public static void performCheckWithdrawal(int r,double g,String h,int f) {
 		CheckWithdrawal03 t=new CheckWithdrawal03(r,g,h,f);
 
 		for (int s=0;s<listOfAccounts.size();s++)
-				{
+		{
 			if (listOfAccounts.get(s).getAcctNum()==t.getAccount()) 
-		      {
-		   Account03   w= listOfAccounts.get(s);
-		         w.setBalance(t.getAmount());
-			 w.addTransaction(t);
-                        break;
-		      }}
+		      	{
+		   		Account03   w= listOfAccounts.get(s);
+		         	w.setBalance(t.getAmount());
+			 	w.addTransaction(t);
+                        	break;
+		      	}
+		}
 
 	}
+	
 	public static String readAccount(int acctNum)
 	{
-	 for (Account03 a:listOfAccounts)
-	 {
-	
-		if (a.getAcctNum()==acctNum) 
-           {
-             return"\n\nAccount holder:"+a.getName()+"\nAccount number:"+
-			          String.valueOf(a.getAcctNum())+
-							String.format("\nBalance:$%,.2f",a.getBalance())+
+	 	for (Account03 a:listOfAccounts)
+	 	{
+			if (a.getAcctNum()==acctNum) 
+           		{
+             			return"\n\nAccount holder:"+a.getName()+"\nAccount number:"+
+			        	String.valueOf(a.getAcctNum())+
+						String.format("\nBalance:$%,.2f",a.getBalance())+
 							"\nTransactions:\n"+ (a.printAll());
-		   }
+		   	}
 		
-	 }
-	return"Account not found";
+	 	}
+		return"Account not found";
 	}
 	 
 	
-
-
-
-
-
-
 	public static void main(String[]args){
      
 		Runtime.getRuntime().addShutdownHook( new Thread()
-		{ public void run(){BankDatabase.store();} } );
+		{ 
+			public void run()
+			{
+				BankDatabase.store();
+			} 
+		} );
 		
 		BankDatabase.startDay();
 		
 		new BankGui();
 	
- }	
+	}	
 	
 }
 
