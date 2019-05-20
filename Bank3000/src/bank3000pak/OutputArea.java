82a973bc31270fe55a6378e7d83959e91632090e
avrahamMayerfeld <44050391+avrahamMayerfeld@users.noreply.github.com>
2019-05-19 22:58:28 -0400

    fixed indentation

--- Bank3000/src/bank3000pak/OutputArea.java
+++ Bank3000/src/bank3000pak/OutputArea.java
@@ -5,20 +5,20 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class OutputArea extends JPanel{
-private JTextArea  area;
-private JScrollPane jsp;
-public OutputArea(){
-area=new JTextArea(30,40);
-area.setLineWrap(true);
-jsp=new JScrollPane(area);
-add(jsp);
-area.setEditable(false);
-}
-public void setArea(String e){
-area.append(e);
-
-}
-
+          private JTextArea  area;
+          private JScrollPane jsp;
+          
+          public OutputArea(){
+                    area=new JTextArea(30,40);
+                    area.setLineWrap(true);
+                    jsp=new JScrollPane(area);
+                    add(jsp);
+                    area.setEditable(false);
+          }
+          
+          public void setArea(String e){
+                    area.append(e);
+          }
 }
 
 
